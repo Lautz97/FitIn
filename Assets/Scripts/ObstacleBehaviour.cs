e5162509b6b9828dc02e5c5fa754d295237f7229
Lautz97 <davide.lauterio@gmail.com>
2021-03-05 09:03:04 +0100

    hello there, first iteration

---
+++ Assets/Scripts/ObstacleBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleBehaviour : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private AnimationClip anim;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - speed * Time.deltaTime);
+    }
+
+    public void SetDimensions(float f)
+    {
+        Animator a = GetComponent<Animator>();
+        a.speed = 0;
+        a.Play(anim.name, 0, f);
+    }
+
+}
---
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private GameObject obstacle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnCycle());
+    }
+
+    private IEnumerator SpawnCycle()
+    {
+        for (int i = 0; i < 1;)
+        {
+            SpawnObstacle(Random.Range(0f, 1f));
+            yield return new WaitForSecondsRealtime(3f);
+        }
+    }
+
+    void SpawnObstacle(float a)
+    {
+        GameObject newObs = Instantiate(obstacle, spawnPoint.position, spawnPoint.rotation);
+        newObs.transform.parent = transform;
+        newObs.GetComponent<ObstacleBehaviour>().SetDimensions(a);
+
+    }
+}
